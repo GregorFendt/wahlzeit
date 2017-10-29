75060eb3d7510cdc33b0c892d89da1ec030caa39
pupudubb <pupudubb@pupudubb.pupudubb>
2017-10-29 22:57:26 +0100

    new classes 03

--- src/main/java/org/wahlzeit/services/Coordinate.java
+++ src/main/java/org/wahlzeit/services/Coordinate.java
@@ -1,4 +1,13 @@
-package main.java.org.wahlzeit.services.mailing;
+/*
+ *   Classname: Coordinate
+ *
+ *   Version: 1.0 [created because of the 2nd ADAP-Homework]
+ *
+ *   Date: 29.10.2017
+ *
+ */
+package main.java.org.wahlzeit.services;
+
 
 public class Coordinate {
 }
--- src/main/java/org/wahlzeit/services/Location.java
+++ src/main/java/org/wahlzeit/services/Location.java
@@ -1,4 +1,15 @@
+/*
+ *   Classname: Location
+ *
+ *   Version: 1.0 [created because of the 2nd ADAP-Homework]
+ *
+ *   Date: 29.10.2017
+ *
+ */
 package main.java.org.wahlzeit.services;
 
 public class Location {
+
+
+
 }
